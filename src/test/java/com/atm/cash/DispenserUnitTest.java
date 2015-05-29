1ceed6f50a1db0d990a379d2bbfc24bf9a6c3726
Wadhwa <wadhwasahil@johndeere.com>
2015-05-29 17:04:41 +0530

    managing constants

--- src/test/java/com/atm/cash/DispenserUnitTest.java
+++ src/test/java/com/atm/cash/DispenserUnitTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static com.atm.cash.DispenserUnit.FIVE_HUNDRED_NOTE_COUNT;
+import static com.atm.cash.DispenserUnit.HUNDRED_NOTE_COUNT;
+import static com.atm.cash.DispenserUnit.THOUSAND_NOTE_COUNT;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
@@ -14,15 +17,16 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class DispenserUnitTest {
 
+    public static final long REQUESTED_CASH = 1000l;
     private DispenserUnit dispenserForTest;
 
     @Test
     public void shouldInitializeDispenserWithCash() throws Exception {
         dispenserForTest = DispenserUnit.getInstance();
         Cash cashInDispenser = dispenserForTest.getCashInDispenser();
-        assertTrue("Incorrect Thousand currency notes initialized", cashInDispenser.get(CurrencyType.THOUSAND).equals(10));
-        assertTrue("Incorrect Five Hundred currency notes initialized", cashInDispenser.get(CurrencyType.FIVE_HUNDERED).equals(10));
-        assertTrue("Incorrect Hundred currency notes initialized", cashInDispenser.get(CurrencyType.HUNDERED).equals(10));
+        assertTrue("Incorrect Thousand currency notes initialized", cashInDispenser.get(CurrencyType.THOUSAND).equals(THOUSAND_NOTE_COUNT));
+        assertTrue("Incorrect Five Hundred currency notes initialized", cashInDispenser.get(CurrencyType.FIVE_HUNDERED).equals(FIVE_HUNDRED_NOTE_COUNT));
+        assertTrue("Incorrect Hundred currency notes initialized", cashInDispenser.get(CurrencyType.HUNDERED).equals(HUNDRED_NOTE_COUNT));
     }
 
     @Test
@@ -31,7 +35,7 @@ public class DispenserUnitTest {
         DispenseChain mockDispenseChain = mock(DispenseChain.class);
         dispenserForTest.setRootDispenser(mockDispenseChain);
         Cash expectedValue = new Cash();
-        Long requestedCash = 1000l;
+        Long requestedCash = REQUESTED_CASH;
         when(mockDispenseChain.dispense(any(Cash.class),eq(dispenserForTest.getCashInDispenser()),eq(requestedCash))).thenReturn(expectedValue);
         Cash actualCash = dispenserForTest.dispenseCash(requestedCash);
         assertSame("Incorrect cash returned",expectedValue, actualCash);
